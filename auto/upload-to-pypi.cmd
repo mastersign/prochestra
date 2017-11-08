80ef7b65c58d57ce63b2bd521da1f3e272c43073
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2017-11-08 13:08:58 +0100

    improved upload scripts

--- auto/upload-to-pypi.cmd
+++ auto/upload-to-pypi.cmd
@@ -1,6 +1,7 @@
 @ECHO OFF
 
 PUSHD "%~dp0.."
+DEL /S /Q "%~dp0..\dist"
 CALL python setup.py sdist bdist_wheel
 CALL twine upload "%~dp0../dist/*"
 POPD
--- auto/upload-to-testpypi.cmd
+++ auto/upload-to-testpypi.cmd
@@ -3,6 +3,7 @@
 SET indexurl=https://test.pypi.org/legacy/
 
 PUSHD "%~dp0.."
+DEL /S /Q "%~dp0..\dist"
 CALL python setup.py sdist bdist_wheel
 CALL twine upload "%~dp0../dist/*" --repository-url %indexurl%
 POPD
